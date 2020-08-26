e28ab72ef6381e79ad76f4ba8acc0c6f1179f136
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-26 13:30:15 -0700

    input and play now allow user to move the snake

--- input.js
+++ input.js
@@ -1,17 +1,33 @@
 const stdin = process.stdin;
 
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
 
   
+  const handleUserInput = stdin.on('data', (key)=>{
+    if (key === 'w') {
+      conn.write("Move: up");
+    } else if (key === 's') {
+      conn.write("Move: down");
+    }
+    if (key === 'a') {
+      conn.write("Move: left");
+    } else if (key === 'd') {
+      conn.write("Move: right");
+    }
+    
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  
   stdin.resume();
   return stdin, handleUserInput;
 };
-const handleUserInput = stdin.on('data', (key)=>{
-  if (key === '\u0003') {
-    process.exit();
-  }
-});
 
-module.exports = {setupInput, handleUserInput};
+module.exports = {setupInput};
--- play.js
+++ play.js
@@ -2,5 +2,6 @@ const {connect}  = require('./client');
 const {setupInput} = require('./input');
 
 console.log("Connecting...");
+const conn = connect();
 connect();
-setupInput();
+setupInput(conn);
