32a29416be19272fc8706171ce03d7ad0622003b
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-26 13:17:11 -0700

    play and client modified

--- client.js
+++ client.js
@@ -1,5 +1,26 @@
-const connect  = require('./play');
-const stdin = process.stdin;
+const net = require('net');
 
-console.log("Connecting...");
-connect();
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('connect', ()=>{
+    console.log('Connection Established');
+  });
+  conn.on('connect', ()=>{
+    conn.write('Name: SRM');
+  });
+  conn.on('data', (data)=> {
+    console.log(data);
+  });
+  return conn;
+};
+
+
+
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,37 +1,6 @@
-const net = require('net');
+const {connect}  = require('./client');
+const {setupInput} = require('./input');
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on('connect', ()=>{
-    console.log('Connection Established');
-  });
-  conn.on('connect', ()=>{
-    conn.write('Name: SRM');
-  });
-  conn.on('data', (data)=> {
-    console.log(data);
-  });
-  return conn;
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  const handleUserInput = stdin.on('data', (key)=>{
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-  stdin.resume();
-  return stdin, handleUserInput;
-};
-
+console.log("Connecting...");
+connect();
 setupInput();
-module.exports = connect;
